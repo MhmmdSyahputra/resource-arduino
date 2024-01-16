ca90381b0c997c25a205c5f28188bb7e16bd6ac4
MhmmdSyahputra <putra.cokers@gmail.com>
2024-01-17 02:00:58 +0700

    add status in distance

--- measure-distance/measure-distance.ino
+++ measure-distance/measure-distance.ino
@@ -4,7 +4,7 @@
 
 const char *ssid = "belik";
 const char *password = "putra12345";
-const char *serverUrl = "https://api-iot-testing.vercel.app/"; // Ganti dengan URL API yang sesuai
+const char *serverUrl = "https://api-iot-testing.vercel.app/";
 
 const int trigPin = 5;
 const int echoPin = 18;
@@ -18,10 +18,10 @@ float distanceCm;
 float distanceInch;
 
 void setup() {
-  Serial.begin(115200); // Starts the serial communication
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-
+  Serial.begin(115200);      // Starts the serial communication
+  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);   // Sets the echoPin as an Input
+  pinMode(2, OUTPUT);        //indikator
   // Connect to WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -39,16 +39,16 @@ void loop() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-  
+
   // Calculate the distance
-  distanceCm = duration * SOUND_SPEED/2;
-  
+  distanceCm = duration * SOUND_SPEED / 2;
+
   // Convert to inches
   distanceInch = distanceCm * CM_TO_INCH;
-  
+
   // Prints the distance in the Serial Monitor
   Serial.print("Distance (cm): ");
   Serial.println(distanceCm);
@@ -56,8 +56,8 @@ void loop() {
   Serial.println(distanceInch);
 
   // Send data to server via API with distance as a query parameter
+  status();
   sendDataToServer();
-
   delay(500);
 }
 
@@ -81,5 +81,48 @@ void sendDataToServer() {
     Serial.printf("HTTP request failed with error: %s\n", http.errorToString(httpCode).c_str());
   }
 
+  http.end();
+}
+
+void status() {
+  // Buat objek HTTPClient
+  HTTPClient http;
+
+  // Atur URL target
+  String url = String(serverUrl) + "getMode";
+  http.begin(url);
+
+  // Kirim permintaan GET
+  int httpResponseCode = http.GET();
+
+  // Cetak hasil permintaan
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+
+  // Baca dan cetak respons dari server
+  String payload = http.getString();
+  Serial.println("Response payload: " + payload);
+
+  // Parse respon JSON menggunakan ArduinoJSON
+  DynamicJsonDocument doc(1024);  // Sesuaikan ukuran dokumen sesuai kebutuhan
+  DeserializationError error = deserializeJson(doc, payload);
+
+  // Periksa kesalahan parsing
+  if (error) {
+    Serial.print("Parsing failed: ");
+    Serial.println(error.c_str());
+  } else {
+    // Ambil nilai dari kunci "mode"
+    int modeValue = doc["mode"];
+
+    // Serial.println(modeValue);
+    if (modeValue == 1) {
+      // Switch ON, hidupkan lampu
+      digitalWrite(2, HIGH);
+    } else {
+      // Switch OFF, matikan lampu
+      digitalWrite(2, LOW);
+    }
+  }
   http.end();
 }
